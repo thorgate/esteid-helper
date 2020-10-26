551771e1dbeed40c01b04fcefc6c441d46a8bef0
Yuriy Shatrov <yks-uno@yandex.ru>
2020-10-26 08:58:26 +0200

    new api

--- IdentificationManager.js
+++ IdentificationManager.js
@@ -1,26 +1,25 @@
-import IdCardManager from './IdCardManager'
+import IdCardManager from "./IdCardManager"
 
-async function request(url, data, method = 'POST') {
+async function request(url, data, method = "POST") {
     try {
         const response = await fetch(url, {
             method: method,
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
-            body: method === 'GET' ? null : JSON.stringify(data || {})
+            body: method === "GET" ? null : JSON.stringify(data || {}),
         })
 
         const body = await response.text()
 
         try {
             const data = JSON.parse(body)
-            data.success = data.status === 'success'
-            data.pending = `${response.status}` === '202'
+            data.success = data.status === "success"
+            data.pending = `${response.status}` === "202"
             return {
                 data,
                 ok: response.ok,
             }
-
         } catch (err) {
             console.log("Failed to parse response as JSON", body)
             return {}
@@ -31,16 +30,8 @@ async function request(url, data, method = 'POST') {
     }
 }
 
-
 class IdentificationManager {
-    constructor({
-                    language,
-                    idUrl,
-                    mobileIdUrl,
-                    smartIdUrl,
-                    csrfToken
-                }) {
-
+    constructor({ language, idUrl, mobileIdUrl, smartIdUrl, csrfToken }) {
         // construct the idCardManager
         this.idCardManager = new IdCardManager(language)
 
@@ -54,8 +45,8 @@ class IdentificationManager {
     checkStatus(endpoint, resolve, reject) {
         console.log("Status", endpoint)
         const doRequest = () => {
-            request(endpoint, null, 'GET')
-                .then(({ok, data}) => {
+            request(endpoint, null, "GET")
+                .then(({ ok, data }) => {
                     if (ok && data.pending) {
                         setTimeout(() => doRequest(), 1000)
                     } else if (ok && data.success) {
@@ -63,66 +54,65 @@ class IdentificationManager {
                     } else {
                         reject(data)
                     }
-                }).catch((err) => {
-                console.log("Status error", err)
-
-            })
-
+                })
+                .catch((err) => {
+                    console.log("Status error", err)
+                })
         }
         return doRequest()
-    };
+    }
 
     signWithIdCard() {
         return new Promise((resolve, reject) => {
-            this.__signHandleId(resolve, reject)
+            this.__signHandleIdCard(resolve, reject)
         })
     }
 
-    signWithMobileId({idCode, phoneNumber}) {
+    signWithMobileId({ idCode, phoneNumber }) {
         return new Promise((resolve, reject) => {
             this.__signHandleMid(idCode, phoneNumber, resolve, reject)
         })
     }
 
-    signWithSmartId({idCode}) {
+    signWithSmartId({ idCode }) {
         return new Promise((resolve, reject) => {
             this.__signHandleSmartid(idCode, resolve, reject)
         })
     }
 
-    __signHandleId(resolve, reject) {
+    __signHandleIdCard(resolve, reject) {
         this.idCardManager.initializeIdCard().then(() => {
             this.idCardManager.getCertificate().then((certificate) => {
                 request(this.idUrl, {
                     csrfmiddlewaretoken: this.csrfToken,
-                    certificate: certificate
+                    certificate: certificate,
+                }).then(({ ok, data }) => {
+                    if (ok && data.success) {
+                        this.__doSign(data.digest, resolve, reject)
+                    } else {
+                        reject(data)
+                    }
                 })
-                    .then(({ok, data}) => {
-                        if (ok && data.success) {
-                            this.__doSign(data.digest, resolve, reject)
-                        } else {
-                            reject(data)
-                        }
-                    })
-
             }, reject)
-
         }, reject)
     }
 
     __doSign(dataDigest, resolve, reject) {
         this.idCardManager.signHexData(dataDigest).then((signature) => {
-            request(this.idUrl, {
-                csrfmiddlewaretoken: this.csrfToken,
-                signature_value: signature
-            }, 'PATCH')
-                .then(({ok, data}) => {
-                    if (ok && data.success) {
-                        resolve(data)
-                    } else {
-                        reject(data)
-                    }
-                })
+            request(
+                this.idUrl,
+                {
+                    csrfmiddlewaretoken: this.csrfToken,
+                    signature_value: signature,
+                },
+                "PATCH"
+            ).then(({ ok, data }) => {
+                if (ok && data.success) {
+                    resolve(data)
+                } else {
+                    reject(data)
+                }
+            })
         }, reject)
     }
 
@@ -131,15 +121,14 @@ class IdentificationManager {
             id_code: idCode,
             phone_number: phoneNumber,
             language: this.language,
-            csrfmiddlewaretoken: this.csrfToken
+            csrfmiddlewaretoken: this.csrfToken,
+        }).then(({ ok, data }) => {
+            if (ok && data.success) {
+                resolve(data)
+            } else {
+                reject(data)
+            }
         })
-            .then(({ok, data}) => {
-                if (ok && data.success) {
-                    resolve(data)
-                } else {
-                    reject(data)
-                }
-            })
     }
 
     midStatus() {
@@ -152,15 +141,14 @@ class IdentificationManager {
         request(this.smartIdUrl, {
             id_code: idCode,
             language: this.language,
-            csrfmiddlewaretoken: this.csrfToken
+            csrfmiddlewaretoken: this.csrfToken,
+        }).then(({ ok, data }) => {
+            if (ok && data.success) {
+                resolve(data)
+            } else {
+                reject(data)
+            }
         })
-            .then(({ok, data}) => {
-                if (ok && data.success) {
-                    resolve(data)
-                } else {
-                    reject(data)
-                }
-            })
     }
 
     smartidStatus() {
@@ -173,6 +161,5 @@ class IdentificationManager {
         return this.idCardManager.getError(err)
     }
 }
-
 
 export default IdentificationManager
--- README.md
+++ README.md
@@ -1,22 +1,41 @@
-esteid-helper
-=============
+# esteid-helper
 
 ### Configure
 
-Pass the backend URLs to `IdentificationManager()`
+```
+IdentificationManager({
+    idUrl: "the url that is used for ID Card signing",
+    mobileIdUrl: "the url that is used for MobileID signing",
+    smartIdUrl: "the url that is used for SmartID signing",
+    language: "A language supported by *ID, one of ENG, EST, LIT, RUS",
+    csrfToken: "the Django csrf token to add to POST requests",
+})
+```
 
-### Initialize signing 
+### Initialize signing
 
-* ID card: `manager.signWithIdCard()`
-* MobileID: `manager.signWithMobileId()`
-* SmartID: `manager.signWithSmartId()`
+- ID card: `manager.signWithIdCard().then(() => {})`;
+- MobileID: `manager.signWithMobileId({idCode, phoneNumber}).then(() => {})`;
+- SmartID: `manager.signWithSmartId({idCode}).then(() => {})`.
 
 The methods accept an object with required data and the `csrfMiddlewareToken` for POST requests to succeed, and return a Promise.
-Data varies depending on what the backend expects.
+
+The methods issue a signature preparation request to the backend with payload dependent on the signature provider.
+- ID card: `certificate`: hex-encoded signer certificate obtained from the ID card through the `hwcrypto.js` library;
+- MobileID: `id_code`, `phone_number` obtained from user input;
+- SmartID: `id_code` obtained from user input.
 
 ### Status
 
-* MobileID: `manager.midStatus()`
-* SmartID: `manager.smartidStatus()`
+- ID card: not necessary. 
+- MobileID: `manager.midStatus()`
+- SmartID: `manager.smartidStatus()`
 
-The methods accept an object with required data and the `csrfMiddlewareToken` for POST requests to succeed, and return a Promise.
+The methods return a Promise.
+
+The methods issue a signature finalization request to the backend with payload dependent on the signature provider.
+- ID card: `signature_value`: hex-encoded signature obtained from the ID card through the `hwcrypto.js` library;
+- MobileID: no data;
+- SmartID: no data.
+
+If the finalization request results in a `202 Accepted` HTTP response, the request will be repeated after a timeout.
